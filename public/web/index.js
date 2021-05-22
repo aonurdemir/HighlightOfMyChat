99f48be41698bc425adbfa0b5f371b61ed7d39ea
Onur Demir <aaonurdemir@gmail.com>
2021-05-22 21:02:33 +0300

    Only speak for custom reward for 1kadin1erkek channel

--- public/web/index.js
+++ public/web/index.js
@@ -249,18 +249,7 @@ ComfyJS.onMessageDeleted = (id, extra) => {
 ComfyJS.onChat = (user, message, flags, self, extra) => {
 	if (message.length > 150) return
 
-	if (extra.customRewardId === 'cf084591-186a-41c7-91e4-27bc043a19f1') {
-		if (sounds.indexOf(message) === -1) {
-			return
-		}
-
-		messageQueue.push({
-			func: 'playSound',
-			playSound: () => playSound('sounds/' + message.toLowerCase() + '.mp3')
-		})
-	}
-
-	if (flags.highlighted) {
+	if (extra.customRewardId === 'b82b17eb-27a2-450a-a1cb-76f4d23c9791') {
 		messageQueue.push({
 			func: 'highlight',
 			highlight: () => highlightThisMessage(user, message, extra)
